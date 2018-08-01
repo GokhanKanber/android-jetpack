6a15146513ce2ce089484ed3bb0beb0955a42750
Gökhan Kanber <GokhanKanber@users.noreply.github.com>
2018-08-01 15:32:46 +0300

    Create NoteDatabase.java

---
+++ architecture/paging_db/src/main/java/com/gokhankanber/android/architecture/paging/model/source/local/NoteDatabase.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2018 Gökhan Kanber
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gokhankanber.android.architecture.paging.model.source.local;
+
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+import android.arch.persistence.room.migration.Migration;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import com.gokhankanber.android.architecture.paging.model.source.local.converter.DateConverter;
+import com.gokhankanber.android.architecture.paging.model.source.local.dao.NoteDao;
+import com.gokhankanber.android.architecture.paging.model.source.local.entity.Note;
+
+@Database(entities = {Note.class}, version = 1)
+@TypeConverters({DateConverter.class})
+public abstract class NoteDatabase extends RoomDatabase
+{
+    private static final String DATABASE_NAME = "note-content";
+    private static NoteDatabase instance;
+    private static final Object LOCK = new Object();
+    public abstract NoteDao noteDao();
+
+    public static NoteDatabase getInstance(Context context)
+    {
+        if(instance == null)
+        {
+            synchronized(LOCK)
+            {
+                if(instance == null)
+                {
+                    instance = Room.databaseBuilder(context.getApplicationContext(), NoteDatabase.class, DATABASE_NAME)
+                            // .addMigrations(MIGRATION1_2)
+                            .build();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    private static final Migration MIGRATION1_2 = new Migration(1, 2)
+    {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase supportSQLiteDatabase)
+        {
+            supportSQLiteDatabase.execSQL("CREATE TABLE x_" + Note.TABLE_NAME);
+        }
+    };
+}
